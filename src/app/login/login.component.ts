ac6fbcc9b215e47254c951a2a53a3fc9097b2beb
Hozz <medo3578@gmail.com>
2017-06-07 19:08:45 +0200

    small fixes in add todo and login ui v1 finished

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,9 @@
+// tslint:disable:curly
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { LoginResponse } from '../models/login-response.model';
 import { DataService } from '../services/data.service';
+import { Response } from '@angular/http';
 
 @Component({
   selector: 'app-login',
@@ -9,27 +11,32 @@ import { DataService } from '../services/data.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  error = '';
+  problem = false;
 
   constructor(
     private auth: AuthService,
     private data: DataService
   ) { }
+
+
   login(form: HTMLFormElement) {
+    const handleError = (err: Response) => {
+      // if the credentials are incorrect notify the user
+      // else notify the user to try again
+      console.log('ERR', err.status);
+      this.problem = true;
+      if (err.status == 401)
+        this.error = 'Incorrect email or password';
+      else
+        this.error = 'please try again';
+    };
+    // register the token >> redirect to dashboard
     this.auth.login(new FormData(form))
       .subscribe(response => {
-
-        // if logged in successfully register the token and redirect to user dashboard
-        if (response.status === 200) {
-          const data: LoginResponse = response.json();
-          this.data.registerToken(data.token);
-        } else if (response.status === 401) {
-          // if the credentials are incorrect notify the user
-        } else {
-
-          // else notify the user to try again
-        }
-      })
-    ;
+        const data: LoginResponse = response.json();
+        this.data.registerToken(data.token);
+      }, handleError);
   }
   ngOnInit() {
   }
--- src/app/services/http-request.service.ts
+++ src/app/services/http-request.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { generateRequestLink, generateOptions } from '../util';
 import { Http } from '@angular/http';
 import { DataService } from './data.service';
-import 'rxjs/add/operator/map';
 
 @Injectable()
 export class HttpRequestService {
@@ -16,12 +15,6 @@ export class HttpRequestService {
     const token = this.data.getToken();
     const url = generateRequestLink(task);
     const options = generateOptions(token, method, body);
-    return this.http.request(url, options)
-      .map(response => {
-        if (response.status !== 200)
-          this.data.renewSession(response.status);
-        else
-          return response;
-      });
+    return this.http.request(url, options);
   }
 }
