bcb2d52d8b96ce2f6755e12536e595a6b363846b
Hozz <medo3578@gmail.com>
2017-06-08 18:40:25 +0200

    cleaned up data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,13 +6,12 @@ import { Subject } from 'rxjs/Subject';
 @Injectable()
 export class DataService {
   private user: LoginResponse;
-  private token: string;
   constructor(
     private router: Router
   ) { }
 
-  getToken() {
-    return this.token;
+  getToken(): string {
+    return this.user.token;
   }
 
   getUser() {
@@ -20,14 +19,13 @@ export class DataService {
   }
 
   storeUser(user: LoginResponse) {
-    this.user = user;
-    // wait for the token then work with it
-    const token$ = new Subject<string>();
+    // wait for the data then work with it
+    const token$ = new Subject<LoginResponse>();
     token$.subscribe((value) => {
-      this.token = value;
+      this.user = value;
       this.router.navigate(['dashboard']);
     });
-    token$.next(this.user.token);
+    token$.next(user);
   }
 
   renewSession() {
