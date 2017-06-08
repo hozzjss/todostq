ab614c1245a2235444f2f8bef0d3f3334a850562
Hozz <medo3578@gmail.com>
2017-06-08 17:33:04 +0200

    models linted

--- src/app/models/create-response.model.ts
+++ src/app/models/create-response.model.ts
@@ -1,7 +1,7 @@
 import { Todo } from './todo.model';
 
 export interface CreateResponse {
-    "status": string,
-    "message": string,
-    "tasks": object
+    status: string,
+    message: string,
+    tasks: object
 }
