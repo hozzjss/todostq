c2d8015c295889ccdd3db7e82237fcdc657b70b1
Hozz <medo3578@gmail.com>
2017-06-10 12:42:10 +0200

    integrated add todo and marking todo as done with DataService

--- src/app/components/add-todo/add-todo.component.ts
+++ src/app/components/add-todo/add-todo.component.ts
@@ -1,12 +1,8 @@
 // tslint:disable:curly
 import { Component, Output, EventEmitter, OnInit } from '@angular/core';
-import { TodosService } from '../../services/todos.service';
-import { CreateResponse } from '../../models/create-response.model';
-import { DataService } from '../../services/data.service';
-import { Response } from '@angular/http';
-import { objectToArray, adjustLayout, genRandomId } from '../../util/util';
+import { adjustLayout } from '../../util/util';
 import { Todo } from '../../models/todo.model';
-import { Subject } from 'rxjs/Subject';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-add-todo',
@@ -14,55 +10,21 @@ import { Subject } from 'rxjs/Subject';
   styleUrls: ['./add-todo.component.scss']
 })
 export class AddTodoComponent implements OnInit {
-  @Output() updateCreated = new EventEmitter<any[]>();
   @Output() cancel = new EventEmitter<any>();
   add = 'Save';
   loading: boolean;
   constructor(
-    private todos: TodosService,
-    private data: DataService
+    private data: DataService,
   ) { }
 
   save(form: HTMLFormElement, input: HTMLInputElement, event: Event) {
-    // this should wait for the todo to be loaded
-    const todoLoading$ = new Subject<Todo>();
-
     // prevent the form from reloading the page
     event.preventDefault();
-    const handleError = (response: Response) => this.data.renewSession();
-    const handleResponse = (response: Response) => {
-      const data: CreateResponse = response.json();
-      // format new todos
-      const newTodos: Todo[] = objectToArray(data.tasks);
-      // sort it
-      const orderedTodos = newTodos.sort((a, b) => a.id - b.id);
-      // get the last element that's been added
-      const lastAddedTodo = orderedTodos[orderedTodos.length - 1];
-      // then push it to the subject that's been waiting for it to load
-      todoLoading$.next(lastAddedTodo);
-    };
-
-    const pushTodo = () => {
-      this.add = 'Keep adding';
-      // get Todo title
-      const todoTitle = input.value;
-      // construct todo being loaded
-      const loadingTodo: Todo = { title: todoTitle, id: genRandomId(), loading: true };
-      // send it to dashboard with a subject
-      // that's waiting for the todo to be loaded
-      this.updateCreated.emit([loadingTodo, todoLoading$]);
-    };
-
-    const sendTodo = () => {
-      pushTodo();
-      // then make the request in the background
-      this.todos.create(new FormData(form)).subscribe(handleResponse, handleError);
-      // clear input for new todo input
-      input.value = '';
-    };
 
     if (input.value.length > 0) {
-      sendTodo();
+      this.data.create(form);
+      // clear input for new todo input
+      input.value = '';
     }
   }
 
--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TodosService } from '../../services/todos.service';
 import { DataService } from '../../services/data.service';
 import { Todo } from '../../models/todo.model';
 import { Response } from '@angular/http';
 import { Subject } from 'rxjs/Subject';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-todo',
@@ -12,25 +13,14 @@ import { Subject } from 'rxjs/Subject';
 })
 export class TodoComponent {
   @Input() todo: Todo;
-  @Output() updateDone = new EventEmitter<any[]>();
   constructor(
-    private todoService: TodosService,
     private data: DataService
   ) { }
 
   markDone() {
-    const handleError = (response: Response) => this.data.renewSession();
     // only if the todo is not marked as done
     if (this.todo.done === 0) {
-      const loaded$ = new Subject<boolean>();
-      // send this todo to done todos list
-      this.todo.loading = true;
-      this.updateDone.emit([this.todo, loaded$]);
-      // send the request then
-      this.todoService.markDone(this.todo.id)
-        // after that's done update the todo to be marked as loaded
-        // the only error here is session expiration so renew session
-        .subscribe(() => loaded$.next(true), handleError);
+      this.data.markDone(this.todo);
     }
   }
 
