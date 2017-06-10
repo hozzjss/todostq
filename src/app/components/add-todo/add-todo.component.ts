ed145bfac88992e9aed2ce1ff6c15dd009963a54
Hozz <medo3578@gmail.com>
2017-06-10 14:30:16 +0200

    add more instead of save after the first new todo is added

--- src/app/components/add-todo/add-todo.component.ts
+++ src/app/components/add-todo/add-todo.component.ts
@@ -25,6 +25,7 @@ export class AddTodoComponent implements OnInit {
       this.data.create(form);
       // clear input for new todo input
       input.value = '';
+      this.add = 'Add more';
     }
   }
 
