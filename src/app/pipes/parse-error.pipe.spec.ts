c67b29f06a7304501b5a6aca7429c9c9c848f847
hozzjss <medo3578@gmail.com>
2017-06-28 10:49:28 +0200

    added pipe spec and removed all the tests from app component

--- src/app/pipes/parse-error.pipe.spec.ts
+++ src/app/pipes/parse-error.pipe.spec.ts
@@ -5,8 +5,19 @@ describe('KeysPipe', () => {
     const pipe = new ParseErrorPipe();
     expect(pipe).toBeTruthy();
   });
-  it('should take an object and return its keys', () => {
+  it('should return only "required" if required exists on the object', () => {
     const pipe = new ParseErrorPipe();
-    expect(pipe.transform({ key1: 'val1', key2: 'val2' })).toBe(['key1', 'key2']);
+    expect(pipe.transform({ pattern: 'val1', required: 'val2', email: 'val3' }))
+    .toBe('Required');
+  });
+  it('should otherwise return "Invalid email address" if email was present', () => {
+    const pipe = new ParseErrorPipe();
+    expect(pipe.transform({ pattern: 'val1', email: 'val3' }))
+      .toBe('Invalid email address');
+  });
+  it('should return "Please enter your real name" if the only value was pattern', () => {
+    const pipe = new ParseErrorPipe();
+    expect(pipe.transform({ pattern: 'val1' }))
+      .toBe('Please enter your real name');
   });
 });
